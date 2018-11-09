5abd2040824348808e4e52c6f52973dabdac4a61
Dmitiry Dronov <bsuir.dmitriy.dronov@gmail.com>
2018-11-10 01:34:19 +0300

    Ubuntu fix

--- SocketLow.cpp
+++ SocketLow.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "SocketLow.h"
 
+#ifndef SOCKET
+#define SOCKET int
+#endif
 
 SocketLow::SocketLow()
 {
@@ -14,7 +17,7 @@ SocketLow::~SocketLow()
 void SocketLow::SetReceiveTimeout(socket_ptr sock)
 {
 	int timeout = 10;
-	if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) < 0)
+        if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) < 0)
 	{
 		cout << __FUNCTION__ << " error" << endl;
 	}
@@ -28,32 +31,32 @@ void SocketLow::SetKeepAlive(socket_ptr sock)
 	alive.keepaliveinterval = 3;
 	alive.keepalivetime = 3000;
 	DWORD ret;
-	DWORD Res = WSAIoctl(*(SOCKET*)(sock->getNativeHandle()), SIO_KEEPALIVE_VALS, &alive, sizeof(alive), NULL, 0, &ret, NULL, NULL);
+        DWORD Res = WSAIoctl(*(SOCKET*)(sock->getNativeHandle()), SIO_KEEPALIVE_VALS, &alive, sizeof(alive), NULL, 0, &ret, NULL, NULL);
 	if (Res == SOCKET_ERROR)
 	{
 		cout << "error with keepalive" << endl;
 	}
 #else
 	int keepalive = 1;
-	if (setsockopt(sock->native_handle(), SOL_SOCKET, SO_KEEPALIVE, &keepalive, (socklen_t)sizeof(keepalive)) < 0)
+        if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), SOL_SOCKET, SO_KEEPALIVE, &keepalive, (socklen_t)sizeof(keepalive)) < 0)
 	{
 
 		perror("setsockopt(keepalive)");
 	}
 	int keepcnt = 1;
-	if (setsockopt(sock->native_handle(), IPPROTO_TCP, TCP_KEEPCNT, &keepcnt, (socklen_t)sizeof(int))<0)
+        if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), IPPROTO_TCP, TCP_KEEPCNT, &keepcnt, (socklen_t)sizeof(int))<0)
 	{
 
 		perror("setsockopt(keepcnt)");
 	}
 	int keepidle = 30;
-	if (setsockopt(sock->native_handle(), IPPROTO_TCP, TCP_KEEPIDLE, &keepidle, (socklen_t)sizeof(int))<0)
+        if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), IPPROTO_TCP, TCP_KEEPIDLE, &keepidle, (socklen_t)sizeof(int))<0)
 	{
 
-		perror("setsockopt(keepidle)");
+                perror("setsockopt(keepidle)");
 	}
 	int keepintvl = 1;
-	if (setsockopt(sock->native_handle(), IPPROTO_TCP, TCP_KEEPINTVL, &keepintvl, (socklen_t)sizeof(int))<0)
+        if (setsockopt(*(SOCKET*)(sock->getNativeHandle()), IPPROTO_TCP, TCP_KEEPINTVL, &keepintvl, (socklen_t)sizeof(int))<0)
 	{
 		perror("setsockopt(keepalive)");
 	}
